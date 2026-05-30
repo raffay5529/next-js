018093af362f1152fd6f10bf780579ad30300352
codingwithharoon <abdulrafay5526@gmail.com>
2026-05-30 22:46:42 +0500

    add trusted origins

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,47 +2,42 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
-import type { User } from "better-auth";
 import { initializeUserBoard } from "../init-user-board";
 
+const client = new MongoClient(process.env.MONGODB_URI!);
 
-const client= new MongoClient(process.env.MONGODB_URI!);
+const db = client.db();
 
-const db=client.db();
-
-export const auth=betterAuth({
-  database:mongodbAdapter(db,{
+export const auth = betterAuth({
+  trustedOrigins: ["https://taska.vercel.app"],
+  database: mongodbAdapter(db, {
     client,
-
   }),
-  session:{
-    cookieCache:{
-      enabled:true,
-      maxAge:60*60,
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60,
     }
   },
-  emailAndPassword:{
-    enabled:true,
+  emailAndPassword: {
+    enabled: true,
   },
-  databaseHooks:{
-    user:{
-      create:{
-        after:async (user)=>{
-          if(user.id){
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          if (user.id) {
             await initializeUserBoard(user.id);
           }
-        
+        }
       }
     }
   }
-}
 });
 
-export async function getSession(){
-  const result=await auth.api.getSession({
-    headers:await headers(),
+export async function getSession() {
+  const result = await auth.api.getSession({
+    headers: await headers(),
   });
   return result;
-};
-
-
+}
